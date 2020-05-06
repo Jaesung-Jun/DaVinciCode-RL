4824e20f3fc24e747ba11015661f8a35a33bc4dc
75002d4e2357cef779d6b8f868342d8a <40080536+Unkn03nUser@users.noreply.github.com>
2020-05-06 13:35:03 +0900

    gui update

--- davinciCode.py
+++ davinciCode.py
@@ -8,6 +8,7 @@ Da vinci code The game (Board Game)
 
 import random
 import time
+import davinciCode as dvcode
 
 #First Order return
 def your_Order(howmanyplayers):
@@ -38,19 +39,19 @@ class Player:
                 temp.append("{}{}".format(self.blocks[i]['num'], self.blocks[i]['color']))
         temp = sorted(temp)
         for i in range(len(temp)):
-            
             if "x" in temp[i]:
                 temp[i] = temp[i].replace("x", "10")
             elif "y" in temp[i]:
                 temp[i] = temp[i].replace("y", "11")
             elif "z" in temp[i]:
                 temp[i] = temp[i].replace("z", "12")
-            
-            if temp[i][:-1] != "J":
+            if temp[i][:-1] != "j":
                 self.blocks[i] = {'color':temp[i][-1:], 'num':int(temp[i][:-1])}
             else:
-                self.blocks[i] = {'color':temp[i][-1:], 'num':temp[i][:-1]}
+                #self.blocks[i] = {'color':temp[i][-1:], 'num':temp[i][:-1]}
+                continue
         return self.blocks
+
 class Game:
 
     black_blocks = []
@@ -63,24 +64,23 @@ class Game:
         time.sleep(1)
         self.black_blocks = [{'color':'b', 'num':0}, {'color':'b', 'num':1}, {'color':'b', 'num':2}, {'color':'b', 'num':3}, {'color':'b', 'num':4},
                         {'color':'b', 'num':5}, {'color':'b', 'num':6}, {'color':'b', 'num':7}, {'color':'b', 'num':8}, {'color':'b', 'num':9},
-                        {'color':'b', 'num':10}, {'color':'b', 'num':11}, {'color':'b', 'num':100}]
+                        {'color':'b', 'num':10}, {'color':'b', 'num':11}, {'color':'b', 'num':'j'}]
 
         self.white_blocks = [{'color':'w', 'num':0}, {'color':'w', 'num':1}, 
                         {'color':'w', 'num':2}, {'color':'w', 'num':3}, {'color':'w', 'num':4}, {'color':'w', 'num':5}, {'color':'w', 'num':6}, 
                         {'color':'w', 'num':7}, {'color':'w', 'num':8}, {'color':'w', 'num':9}, {'color':'w', 'num':10}, {'color':'w', 'num':11}, 
-                        {'color':'w', 'num':100}]
+                        {'color':'w', 'num':'j'}]
 
         self.blocks = self.black_blocks + self.white_blocks
     #Hand out 26 blocks when game start
-    def random_Block_To_Player(self, player, block_num):
-        for i in range(block_num):
+    def random_Block_To_Player(self, player, howmany):
+        for i in range(howmany):
             while(True):
                 random.shuffle(self.blocks)
-                if self.blocks[0]['num'] != 100:
+                if self.blocks[0]['num'] != 'j':
                     player.blocks.append(self.blocks[0])
                     del self.blocks[0]
                     break
-
 
     def random_Block_Pick(self, player):
         if len(self.blocks) != 0:
@@ -89,12 +89,12 @@ class Game:
             ret_block = self.blocks[0]
             del self.blocks[0]
             return ret_block
-            
-
     def how_Many_Blocks(self):
         return len(self.blocks)
+
+"""
 if __name__ == '__main__':
-    """
+    
     while(True):
         players = int(input("How many Players(2-4)?"))
         if players > 1 and players < 5:
@@ -103,7 +103,7 @@ if __name__ == '__main__':
             break
         else:
             print("Please select number between 2 and 4.")
-    """
+
     players = 4
     player_list = [n for n in range(players)]
     first = your_Order(players-1)
@@ -121,4 +121,4 @@ if __name__ == '__main__':
     print(player_list[0].show_Blocks())
     print(player_list[0].sort_Blocks())
     game.random_Block_Pick(player_list[0])
-    print(player_list[0].show_Blocks())
+    print(player_list[0].show_Blocks())"""
--- pygame-gui.py
+++ pygame-gui.py
@@ -1,9 +1,8 @@
 import pygame as pg
+import davinciCode as dvcode
 
-pg.init()
-
-SIZE_X = 900
-SIZE_Y = 600
+SIZE_X = 800
+SIZE_Y = 800
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -14,29 +13,74 @@ screen = pg.display.set_mode(size)
 pg.display.set_caption("[Davinci Code The Game (Board Game) Reinforcement Learning Project] by Jaesung-Jun")
     
 class Blocks():
-    def draw_blocks(self, x, y, block):
+    def draw_blocks(self, x, y, block, angle=0):
         image = pg.image.load("imgs/{}{}.png".format(block['num'], block['color'])).convert_alpha()
+        image = pg.transform.scale(image, (50, 70))
+        image = pg.transform.rotate(image, angle)
         screen.blit(image, (x, y))
 
 def main():
 
-   
     draw = Blocks()
 
     clock = pg.time.Clock()
     done = False
 
+    players = 4
+    player_list = [n for n in range(players)]
+    first = dvcode.your_Order(players-1)
+    print("first order of player : {}".format(player_list[first]))
+    for i in range(len(player_list)):
+        player_list[i] = dvcode.Player(player_list[i])
+
+    game = dvcode.Game()
+    #Hand out 26 blocks when game start
+    for i in range(len(player_list)):
+        game.random_Block_To_Player(player_list[i], 3)
+        print("{}번 : {}".format(i, player_list[i].blocks))
+
     while not done:
+        jocker = None
+        jocker_block = None
         clock.tick(10)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 done=True
+            if event.type == pg.KEYUP:
+                block = game.random_Block_Pick(player_list[0])
+                if block['num'] == 'j':
+                    jocker_block = block
+                    while(True):
+                        jocker = input("Where do you want to insert your jocker({}-{})?".format(1, len(player_list[0].blocks)))
+                        if  int(jocker) > len(player_list[0].blocks) and int(jocker) < 0:
+                            print("Jocker should be in range {}-{}".format(1, len(player_list[0].blocks)))
+                        else:
+                            print(jocker_block)
         screen.fill(BACKGROUND)
 
-        block = [{'color':'w', 'num':10}, {'color':'w', 'num':9}, {'color':'w', 'num':8}, {'color':'w', 'num':7}]
-        for i in range(len(block)):
-            draw.draw_blocks(SIZE_X/(i+1),SIZE_Y-200, block[i])
-            
+        player_blocks = []
+
+        #player_blocks list
+        for i in range(len(player_list)):
+            player_list[i].blocks = player_list[i].sort_Blocks()
+            if jocker != None:
+                player_list[i].blocks.insert(int(jocker)-1, jocker_block)
+            player_blocks.append(player_list[i].blocks)
+        
+        #Player 1
+        for i in range(len(player_blocks[0])):
+            draw.draw_blocks((((SIZE_X-250)/(len(player_blocks[0])+1))*(i+1))+90,SIZE_Y-150, player_blocks[0][i], 0)
+        #Player 2
+        for i in range(len(player_blocks[1])):
+            draw.draw_blocks((((SIZE_X-250)/(len(player_blocks[1])+1))*(i+1))+90,SIZE_Y-750, player_blocks[1][i], -180)
+
+        #Player 3
+        for i in range(len(player_blocks[2])):
+            draw.draw_blocks(SIZE_X-150,(((SIZE_Y-250)/(len(player_blocks[2])+1))*(i+1))+90, player_blocks[2][i], 90)
+        #Player 4
+        for i in range(len(player_blocks[3])):
+            draw.draw_blocks(SIZE_X-750,(((SIZE_Y-250)/(len(player_blocks[3])+1))*(i+1))+90, player_blocks[3][i], -90)
+
         pg.display.update()
 
 if __name__ == "__main__":
